59fc925474ff352bef8241bbedd1c18977313c7e
eyad1elmaqtel1-boop <eyad1elmaqtel1@gmail.com>
2026-05-29 20:58:49 -0700

    Update bot activity message to reflect server info

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "CPVP is a Minecraft Server where you try to become rich by selling any item in the game.",
         // Activity type number (0 = Playing).
         type: 0, 
       },
